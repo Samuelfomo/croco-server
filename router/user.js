c13647613d410555b776e94295716eb418d6c29e
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-03-04 18:33:29 +0100

    save u

--- router/user.js
+++ router/user.js
@@ -1,26 +1,234 @@
 const express = require('express');
-const path = require('path');
-const paths = require('../config/paths');
 const {User} = require("../lib/class/User");
-const R = require(path.join(paths.TOOL_DIR, 'Reply'));
-const W = require(path.join(paths.TOOL_DIR, 'Watcher'));
+const {Profil} = require("../lib/class/Profil");
+const {Contact} = require("../lib/class/Contact");
+const W = require("../lib/tool/Watcher");
+const R = require("../lib/tool/Reply");
 
 const router = express.Router();
 
-router.put('/login', async(req, res) => {
+router.put('/check', async(req, res) => {
     try {
-        const {code, pin} = req.body;
-
-        if(!code || !code){
-            return R.handleError(res, W.errorMissingFields, 400)
+        const {createdBy} = req.body;
+        if (!createdBy)
+        {
+            return R.handleError(res, W.errorMissingFields, 400);
         }
-        const  user = new  User(code, pin, null, null, null, null);
-        const entry = await user.verify()
+        const userAdmin = new User(null, null, createdBy, null, null, null, null, null, null, null, null);
+        const existCreatedBy = await userAdmin.getUserManager();
+        if(!existCreatedBy){
+            return R.handleError(res, 'manager_not_found', 404);
+        }
+
+        return R.response(true, existCreatedBy.toJson(), res, 200);
+    }
+    catch (error){
+        return R.handleError(res, error.message, 500);
+    }
+});
+router.put('/validate', async(req, res) => {
+    try {
+        const {managerGuid, userGuid} = req.body;
+
+        if (!managerGuid || !userGuid){
+            return R.handleError(res, W.errorMissingFields, 400);
+        }
+
+        const managerData = new User(null, managerGuid, null, null, null, null, null, null, null, null, null);
+        const existManager = await managerData.getByGuid();
+        if(!existManager){
+            return R.handleError(res, 'manager_not_found', 404);
+        }
+        const partnerData = new User(null, userGuid, null, null, null, null, null, null, null, null, null);
+        const existPartner = await partnerData.getByGuid();
+        if(!existPartner){
+            return R.handleError(res, 'partner_not_found', 404);
+        }
+        if(Number(existPartner.createdBy.id) === Number(existManager.id)){
+            const updatePartner = await User.update(userGuid);
+            return R.response(true, updatePartner.toJson(), res, 200);
+        }
+        return R.handleError(res, 'Permission_denied', 403);
+    }
+    catch (error){
+        return R.handleError(res, error.message, 500);
+    }
+});
+
+router.put('/blocked', async(req, res) => {
+    try {
+        const {managerGuid, userGuid} = req.body;
+
+        if (!managerGuid || !userGuid){
+            return R.handleError(res, W.errorMissingFields, 400);
+        }
+        const managerData = new User(null, managerGuid, null, null, null, null, null, null, null, null, null);
+        const existManager = await managerData.getByGuid();
+        if(!existManager){
+            return R.handleError(res, 'manager_not_found', 404);
+        }
+        const partnerData = new User(null, userGuid, null, null, null, null, null, null, null, null, null);
+        const existPartner = await partnerData.getByGuid();
+        if(!existPartner){
+            return R.handleError(res, 'partner_not_found', 404);
+        }
+        if(Number(existPartner.createdBy.id) === Number(existManager.id)){
+            const updatePartner = await User.blocked(userGuid);
+            return R.response(true, updatePartner.toJson(), res, 200);
+        }
+        return R.handleError(res, 'Permission_denied', 403);
+    }
+    catch (error){
+        return R.handleError(res, error.message, 500);
+    }
+});
+
+router.put('/delete', async(req, res) => {
+    try {
+        const {userGuid} = req.body;
+
+        if (!userGuid){
+            return R.handleError(res, W.errorMissingFields, 400);
+        }
+        const partnerData = new User(null, userGuid, null, null, null, null, null, null, null, null, null);
+        const existPartner = await partnerData.getByGuid();
+        if(!existPartner){
+            return R.handleError(res, 'Permission_denied', 403);
+        }
+            const updatePartner = await User.deleted(userGuid);
+            return R.response(true, updatePartner.toJson(), res, 200);
+    }
+    catch (error){
+        return R.handleError(res, error.message, 500);
+    }
+});
+router.get('/mypartner', async(req, res) => {
+    try {
+        const {managerGuid} = req.body;
+
+        if (!managerGuid){
+            return R.handleError(res, W.errorMissingFields, 400);
+        }
+        const managerData = new User(null, managerGuid, null, null, null, null, null, null, null, null, null);
+        const existManager = await managerData.getByGuid();
+        if(!existManager){
+            return R.handleError(res, 'manager_not_found', 404);
+        }
+            const allPartner = await User.getAllPartner(existManager.id);
+        if (!allPartner){
+            return R.handleError(res, 'Your_have_not_partner', 404);
+        }
+            return R.response(true, allPartner, res, 200);
+    }
+    catch (error){
+        return R.handleError(res, error.message, 500);
+    }
+});
+
+router.post('/add', async(req, res) => {
+    try {
+        const {guid, profil, contact, createdBy} = req.body;
+
+        if (!contact || !createdBy){
+            return R.handleError(res, W.errorMissingFields, 400);
+        }
+
+        const contactData = new Contact(null, contact, null, null, null, null, null, null, null, null)
+        const contactResponse = await  contactData.getByGuid();
+        if (!contactResponse){
+            return R.handleError(res, 'contac_not_found', 404);
+        }
+
+        const createdByData = new User(null, createdBy, null, null, null, null, null, null, null, null, null)
+        const createdByResponse = await  createdByData.getByGuid();
+        if (!createdByResponse){
+            return R.handleError(res, 'manager_not_found', 404);
+        }
+
+        const profilData = new Profil(null, null, profil, null)
+        const profilResponse = await profilData.getByGuid();
+
+        if (!profilResponse){
+            return R.handleError(res, 'profil_not_found', 404);
+        }
+
+        const userData = new User(null, guid, null, null, profilResponse, contactResponse, false, false, createdByResponse, false, false);
+
+        const entry = await userData.save();
         return R.response(true, entry.toJson(), res, 200);
     }
     catch (error){
-        return R.handleError(res, "internal_server_error", 500);
+        return R.handleError(res, error.message, 500);
     }
-})
+});
+
 
 module.exports = router;
+
+
+// const express = require('express');
+// const path = require('path');
+// const paths = require('../config/paths');
+// const {User} = require("../lib/class/User");
+// const {Profil} = require("../lib/class/Profil");
+// const R = require(path.join(paths.TOOL_DIR, 'Reply'));
+// const W = require(path.join(paths.TOOL_DIR, 'Watcher'));
+//
+// const router = express.Router();
+//
+// router.put('/check', async(req, res) => {
+//     try {
+//         const {createdBy} = req.body;
+//         const userAdmin = new User(null, createdBy, null, null, null, null, null, null, null, null, null);
+//         const existCreatedBy = await userAdmin.getUserManager();
+//         if(!existCreatedBy){
+//             return R.handleError(res, 'user_not_found', 404);
+//         }
+//
+//         return R.response(true, existCreatedBy.toJson(), res, 200);
+//     }
+//     catch (error){
+//         return R.handleError(res, error.message, 500);
+//     }
+// })
+//
+// router.post('/add', async(req, res) =>{
+//     try {
+//         const {guid, profil, contact, createdBy} = req.body;
+//
+//         if (!contact || !createdBy){
+//             return R.handleError(res, W.errorMissingFields, 400);
+//         }
+//         const profilData = new Profil(null, null, profil, null)
+//         const profilResponse = await profilData.getByGuid();
+//
+//         if (!profilResponse){
+//             return R.handleError(res, 'profil_not_found', 404);
+//         }
+//
+//         const userData = new User(null, guid, null, null, profilResponse, contact, null, null, createdBy, null, null);
+//         const userResponse = userData.save();
+//         return R.response(true, userResponse.toJson(), res, 200);
+//     }
+//     catch (error){
+//         return R.handleError(res, error.message, 500);
+//     }
+// })
+//
+// router.put('/login', async(req, res) => {
+//     try {
+//         const {code, pin} = req.body;
+//
+//         if(!code || !code){
+//             return R.handleError(res, W.errorMissingFields, 400)
+//         }
+//         const  user = new  User(code, pin, null, null, null, null);
+//         const entry = await user.verify()
+//         return R.response(true, entry.toJson(), res, 200);
+//     }
+//     catch (error){
+//         return R.handleError(res, "internal_server_error", 500);
+//     }
+// })
+//
+// module.exports = router;
